b02bca82c0dfdeb86aa9f7c00b52a4eb0cbacbda
thedemonsid <siddheshshrirame@gmail.com>
2023-11-30 01:12:51 +0530

    making carasaul

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 function SecondNav() {
     const items = [
         "Developement",
@@ -27,7 +28,7 @@ function Navbar() {
                 <div className="flex justify-around ">
                     <div className=" text-xl">udemy</div>
                     <div>categories</div>
-                    <div><input className="px-3 border border-slate-700 w-150 "type="text" placeholder="Search"></input></div>
+                    <div><input className="px-3 border border-slate-700 w-150 " type="text" placeholder="Search"></input></div>
                     <div>Udemy Business</div>
                     <div>Teach on Udemy</div>
                     <div>My Learning</div>
@@ -37,14 +38,53 @@ function Navbar() {
                     <div className="bg-black-400 ">SS</div>
                 </div>
                 <hr></hr>
-                 <div className="flex justify-center">
-                 <SecondNav></SecondNav>
-                 </div>
+                <div className="flex justify-center">
+                    <SecondNav></SecondNav>
+                </div>
                 <hr></hr>
             </nav>
 
         </>
     )
+}
+
+function Carasaul() {
+    const images = [
+        "https://jbsoftware.ca/wp-content/uploads/web-design.jpg",
+        "https://img.freepik.com/premium-vector/isometric-landing-page-template-web-development_9209-2328.jpg?w=1800",
+    ];
+    const left = "<";
+    const right = ">";
+    const num = images.length;
+
+    const [count, setCount] = useState(0);
+
+    const handleLeftClick = () => {
+        setCount((prevCount) => (prevCount - 1 + num) % num);
+    };
+
+    const handleRightClick = () => {
+        setCount((prevCount) => (prevCount + 1) % num);
+    };
+
+    return (
+        <>
+            <br></br>
+            <div className="flex justify-center items-center relative">
+                <div className="w-10 text-center text-white bg-black rounded-full hover:bg-slate-700">
+                    <button className="text-2xl" onClick={handleLeftClick}>
+                        {left}
+                    </button>
+                </div>
+                <img className="w-1/3" src={images[count]} alt="tech"></img>
+                <div className="w-10 text-center text-white bg-black rounded-full hover:bg-slate-700">
+                    <button className="text-2xl" onClick={handleRightClick}>
+                        {right}
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
 export default function App() {
     return (
@@ -53,6 +93,7 @@ export default function App() {
                 Hello world!
             </h1>
             <Navbar></Navbar>
+            <Carasaul></Carasaul>
         </>
     )
 }
