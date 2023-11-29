a8c8dfb1938f1b20b2b22b3565792d6ccd35f59a
thedemonsid <siddheshshrirame@gmail.com>
2023-11-29 19:48:37 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LinkedList from './LinkedList';
-
+import "./App"
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
-    <LinkedList></LinkedList>
+    <App></App>
 );
 
 // If you want to start measuring performance in your app, pass a function
